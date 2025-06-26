cacc26a7ed38ebb379e37f50660029dffe55bc4a
VenkatapathiRajuSagi <vpr.sagi@gmail.com>
2025-06-26 18:48:00 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const typed = new Typed('.text', {
+    strings: ["a Developer", "a Designer", "a CSE Student"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+});
